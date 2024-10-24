59472743251502e90347f39a0327ae38ecae8602
Damien Westerman <damien.westerman@snhu.edu>
2024-10-24 12:54:19 -0600

    Initial commit - spring initializer

---
+++ src/main/java/com/damienwesterman/defensedrill/config_server/DefenseDrillConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.damienwesterman.defensedrill.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseDrillConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseDrillConfigServerApplication.class, args);
+	}
+
+}
